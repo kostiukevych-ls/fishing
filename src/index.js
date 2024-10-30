e88a3b21a9eebdef2cc54d798042a9739c22ae2a
Serhii Kostiukevych <serhii.kostiukevych@lembergsolutions.com>
2024-10-30 18:01:34 +0200

    Prepared local env for react

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./app.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './app.jsx';
 import Socket from "./component/socket.jsx";
+import { bootstrap } from "https://cdn.applura.com/dist/js/client/v2.js";
 
-// The "@applura/client" package identifier is defined by the import map in index.html. It is not published on NPM.
-// See https://github.com/Applura/client#browser-only-import.
-import { bootstrap } from "@applura/client";
-
-// Mount and launch the client-side application.
-document.addEventListener("DOMContentLoaded", async () => {
-  // Bootstrap the Applura client. This should wait for the "DOMContentLoaded" event since the "bootstrap" function
-  // depends on <link> elements in the document <head>. They provide the URL for the current resource.
-  const client = bootstrap();
-  // Get the application container.
-  const container = document.getElementById("app");
-  // "Plug" the main application into the Applura client.
-  const Main = <Socket App={App} client={client} />;
-  // Launch the application.
-  createRoot(container).render(Main);
-});
+// Bootstrap the Applura client. This should wait for the "DOMContentLoaded" event since the "bootstrap" function
+// depends on <link> elements in the document <head>. They provide the URL for the current resource.
+const client = bootstrap();
+// "Plug" the main application into the Applura client.
+const container = document.getElementById("app");
+// "Plug" the main application into the Applura client.
+const Main = <Socket App={App} client={client} />;
+// Launch the application.
+ReactDOM.createRoot(container).render(Main);
